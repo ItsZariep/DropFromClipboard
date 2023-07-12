56c31ae77544638b172bd3bc9cf682d2f3825d2d
ItzSelenux <ItzSelenux@zariep.com>
2023-07-11 22:45:11 -0600

    renamed for convenience

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
-//Programmed por ItzSelenux
+//Programmed by ItzSelenux
 let historySize;
 let imgSize;
 
-function loadStoredImages() {
+function loadStoredImages() 
+{
 	const imgContainer = document.getElementById('image-container');
 	const storedImages = localStorage.getItem('storedImages');
 
-	if (storedImages) {
+	if (storedImages) 
+	{
 		const images = JSON.parse(storedImages);
-		for (const imageSrc of images) {
+		for (const imageSrc of images) 
+		{
 			const img = document.createElement('img');
 			img.src = imageSrc;
 			img.style.maxWidth = imgSize + '%';
@@ -17,24 +20,31 @@ function loadStoredImages() {
 	}
 }
 
-async function showClipboardImage() {
-	try {
+async function showClipboardImage() 
+{
+	try 
+	{
 		const clipboardItems = await navigator.clipboard.read();
 		const imgContainer = document.getElementById('image-container');
 
-		for (const clipboardItem of clipboardItems) {
-			for (const type of clipboardItem.types) {
-				if (type === 'image/png') {
+		for (const clipboardItem of clipboardItems) 
+		{
+			for (const type of clipboardItem.types) 
+			{
+				if (type === 'image/png') 
+				{
 					const blob = await clipboardItem.getType(type);
 					const reader = new FileReader();
-					reader.onloadend = function () {
+					reader.onloadend = function () 
+					{
 						const base64Data = reader.result;
 						const img = document.createElement('img');
 						img.src = base64Data;
 						img.style.maxWidth = imgSize + '%';
 						imgContainer.insertBefore(img, imgContainer.firstChild);
 						const images = imgContainer.getElementsByTagName('img');
-						while (images.length > historySize) {
+						while (images.length > historySize) 
+						{
 							imgContainer.removeChild(images[images.length - 1]);
 						}
 					};
@@ -42,18 +52,18 @@ async function showClipboardImage() {
 				}
 			}
 		}
-
-		// Guardar imágenes en el LocalStorage
 		const images = Array.from(imgContainer.getElementsByTagName('img')).map((img) => img.src);
 		localStorage.setItem('storedImages', JSON.stringify(images));
 	} catch (err) {}
 }
 
-function init() {
+function init() 
+{
 	const darkModeCheckbox = document.getElementById('dark-mode-checkbox');
 	const body = document.body;
 
-	darkModeCheckbox.addEventListener('change', function () {
+	darkModeCheckbox.addEventListener('change', function () 
+	{
 		body.classList.toggle('dark-mode', this.checked);
 	});
 
@@ -61,26 +71,30 @@ function init() {
 	button.addEventListener('click', showClipboardImage);
 
 	const storedHistorySize = localStorage.getItem('historySize');
-	if (storedHistorySize) {
+	if (storedHistorySize) 
+	{
 		document.getElementById('history-size').value = storedHistorySize;
 		historySize = parseInt(storedHistorySize);
 	}
 
 	const storedImgSize = localStorage.getItem('imgSize');
-	if (storedImgSize) {
+	if (storedImgSize) 
+	{
 		document.getElementById('img-size').value = storedImgSize;
 		imgSize = parseInt(storedImgSize);
 	}
 
 	const storedDarkModeEnabled = localStorage.getItem('darkModeEnabled');
-	if (storedDarkModeEnabled) {
+	if (storedDarkModeEnabled) 
+	{
 		darkModeCheckbox.checked = storedDarkModeEnabled === 'true';
 		body.classList.toggle('dark-mode', darkModeCheckbox.checked);
 	}
 
 	loadStoredImages();
 
-	setInterval(() => {
+	setInterval(() => 
+	{
 		historySize = parseInt(document.getElementById('history-size').value);
 		imgSize = parseInt(document.getElementById('img-size').value);
 		localStorage.setItem('historySize', historySize);
@@ -89,7 +103,8 @@ function init() {
 		const imgContainer = document.getElementById('image-container');
 		const images = imgContainer.getElementsByTagName('img');
 		imgSize = parseInt(document.getElementById('img-size').value);
-		for (const image of images) {
+		for (const image of images) 
+		{
 			image.style.maxWidth = imgSize + '%';
 		}
 
